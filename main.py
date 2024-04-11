83145c12f1ce3b91277b321d4f0e8c437d125f33
Justin Kunimune <justinkunimune@gmail.com>
2024-04-10 20:30:16 -0400

    Richardson–Lucy!
    it's real now, and it works about as well as I would expect from a fake Bayesian technike.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ To view a copy of this license, visit <https://creativecommons.org/publicdomain/
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
-from scipy import signal, integrate
+from scipy import signal
 
 
 def main():
@@ -13,14 +13,25 @@ def main():
 
 	x_kernel = np.linspace(0, 6, 201)
 	Δx = x_kernel[1] - x_kernel[0]
-	y_kernel = shoe_curve(x_kernel, 0.5, 1.0, +0.8, 10000)
+	y_kernel = shoe_curve(x_kernel, 0.5, 1.0, +0.8, 1)
 
 	x_source = np.linspace(0, 3, 101)
-	y_source = bell_curve(x_source, 0.3, 0.6, 4) + bell_curve(x_source, 0.7, 0.8, 2) + shoe_curve(x_source, 2.4, 1.2, -0.6, 3)
+	y_source = 1000*(
+			bell_curve(x_source, 0.3, 0.6, 4) +
+			bell_curve(x_source, 0.7, 0.8, 2) +
+			shoe_curve(x_source, 2.4, 1.2, -0.6, 3)
+	)
 
 	x_image = np.concatenate([x_kernel[:-1], x_kernel[-1] + x_source])
-	y_image = signal.convolve(y_source, y_kernel*Δx, mode="full")
-	y_data = np.random.poisson((y_image[0:-1] + y_image[1:])/2*Δx)
+
+	# define the point-spread function as a matrix
+	source_to_image = np.transpose(
+		[signal.convolve(y, y_kernel, mode="full")*Δx for y in np.eye(x_source.size)]
+	)
+	source_to_data = (source_to_image[0:-1] + source_to_image[1:])/2
+	source_to_image = source_to_image/np.sum(source_to_data, axis=0, keepdims=True)
+	source_to_data = source_to_data/np.sum(source_to_data, axis=0, keepdims=True)
+	y_data = np.random.poisson(source_to_data @ y_source)
 
 	fig, (image_ax, kernel_ax, source_ax) = plt.subplots(3, 1, facecolor="none")
 
@@ -38,16 +49,22 @@ def main():
 
 	plt.tight_layout()
 
-	source_fit, = source_ax.plot(x_source, np.zeros_like(y_source), color="C1")
-	image_fit, = image_ax.plot(x_image, np.zeros_like(y_image), color="C1")
+	# then do the animation part
+	source_fit, = source_ax.plot(x_source, np.zeros_like(x_source), color="C1")
+	image_fit, = image_ax.plot(x_image, np.zeros_like(x_image), color="C1")
 	label = image_ax.text(0.95, 0.90, "",
 	                      horizontalalignment="right",
 	                      verticalalignment="top",
 	                      transform=image_ax.transAxes)
-	y_guess = np.full(x_source.shape, np.sum(y_data)/integrate.trapezoid(y_kernel, x_kernel)/(x_source[-1] - x_source[0]))
-	for i in range(6):
-		source_fit.set_ydata(y_guess)
-		image_fit.set_ydata(signal.convolve(y_guess, y_kernel*Δx, mode="full")*Δx)
+	y_source_guess = np.full(x_source.shape, np.sum(y_data)/np.sum(y_kernel)/(x_source[-1] - x_source[0]))
+	for i in range(100):
+		# refresh the values dependent on the current source guess
+		y_image_guess = source_to_image @ y_source_guess
+		y_data_guess = source_to_data @ y_source_guess
+
+		# update the plot
+		source_fit.set_ydata(y_source_guess)
+		image_fit.set_ydata(y_image_guess)
 		if i == 0:
 			label.set_text(f"Initial guess")
 		elif i == 1:
@@ -55,8 +72,9 @@ def main():
 		else:
 			label.set_text(f"{i} iterations")
 
-		y_guess = (y_guess + y_source)/2
-		plt.pause(1)
+		# Richardson–Lucy step
+		y_source_guess = y_source_guess * (source_to_data.T @ (y_data/y_data_guess))
+		plt.pause(.1)
 
 	plt.show()
 
